90a6c283b3a13d1dc3906fd35af4a7008ff30a14
Chanse Taylor <Chansen.taylor@gmail.com>
2019-11-10 19:41:47 -0500

    Added a screenshot to the images folder.  Added game.js that will link to my game.  Adjusted app.js to serve static content related to the index.html of /game

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const resumeRouter = require("./routes/resume");
+const gameRouter = require("./routes/game");
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use("/resume", resumeRouter);
+
+app.use("/game", express.static(path.join(__dirname, "views/Adventure-Game")));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next)
