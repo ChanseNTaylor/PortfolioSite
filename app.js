2f722ed7c7f9f83b63dc7ab03888f21c06085d5f
Chanse Taylor <Chansen.taylor@gmail.com>
2019-11-20 14:28:52 -0500

    Styling the container for each project and its links

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use("/resume", resumeRouter);
-app.use("/game", express.static(path.join(__dirname, "views/externals/Adventure-Game")));
+app.use("/game", express.static(path.join(__dirname, "views/externals/adventure-game")));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) { next(createError(404)); });
